74bdb1b1376cf2b7fc15487db1489fdc2b07566a
lvzr <luxoresl@gmail.com>
2024-09-23 18:14:48 +0200

    added init.cmd

---
+++ ps.profile/init.cmd
@@ -0,0 +1,4 @@
+@echo off
+powershell.exe -NoProfile -Command "& {Start-Process pwsh.exe -ArgumentList '-Command \"\$Env:KOMOREBI_CONFIG_HOME = ''.config\komorebi''; komorebic start --ffm\"' -WindowStyle Hidden }"
+pow{Start-Process pwsh.exe -ArgumentList '-Command ""buttery-taskbar""' -WindowStyle Hidden}"
+exitershell.exe -NoProfile -Command "& 
