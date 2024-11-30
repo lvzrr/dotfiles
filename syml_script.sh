79ff2a95a3a14f02c0fd79e2ad35fdd23b1a3b51
lvzrr <luxoresl@gmail.com>
2024-11-30 16:54:40 +0100

    made a lil script to create symlinks to help management

---
+++ syml_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONF_FOLDER="$HOME/.config/"
+
+for item in *; do
+    if [ $item != "syml_script.sh" ]; then
+        sym="$CONF_FOLDER$item"
+        echo "CREATING SYMLINK FOR $item"
+        rm -fr "$sym"
+        ln -s -f "$PWD/$item" "$sym"
+        echo "link created [$item -> $sym]"
+    fi
+done
