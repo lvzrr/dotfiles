9e42672043f9c1011f00850aa16bda71c49bcb18
lvzrr <luxoresl@gmail.com>
2024-11-30 17:05:25 +0100

    support for .* files

--- syml_script.sh
+++ syml_script.sh
@@ -2,9 +2,17 @@
 
 CONF_FOLDER="$HOME/.config/"
 
-for item in *; do
+for item in .* *; do
     if [ $item != "syml_script.sh" ]; then
         sym="$CONF_FOLDER$item"
+        echo "CREATING SYMLINK FOR $item"
+        rm -fr "$sym"
+        ln -s -f "$PWD/$item" "$sym"
+        echo "link created [$item -> $sym]"
+    fi
+
+    if [ "$item" == ".zshrc" ]; then
+        sym="$HOME/$item"
         echo "CREATING SYMLINK FOR $item"
         rm -fr "$sym"
         ln -s -f "$PWD/$item" "$sym"
